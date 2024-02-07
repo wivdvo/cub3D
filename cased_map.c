02f21ae122deac96c36d86af10e1dc3418eddaf1
Filip Seles <fseles@c2r5p6.42vienna.com>
2024-02-07 19:28:23 +0100

    first test on left

--- cased_map.c
+++ cased_map.c
@@ -32,6 +32,72 @@ int	is_valid_char(char c)
 	return (0);
 }
 
+//check if c is one of 0 N S E W
+//if yes that means that for that character check of closed map is needed
+//return 1 if char c need check
+//return 0 if it doesnt
+int need_check(char c)
+{
+	const int need_check[5] = {'0', 'N', 'S', 'E', 'W'};
+	int i;
+
+	i = 0;
+	while(i < 5)
+	{
+		if(c == (char)need_check[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+//if character is space newline or back0
+//return 1
+//if not retur 0;
+int is_end_map_char(char c)
+{
+	const int end_map_char[3] = {' ', '\n', '\0'};
+	int i;
+
+	i = 0;
+	while(i < 3)
+	{
+		if(c == (char)end_map_char[i])
+			return(1);
+		i++;
+	}
+	return (0);
+}
+
+//check if there is wall on right of player or 0
+//return 1 if there is 
+//return 0 if there is no wall
+int right_check(char dot, int row, int column, char **map)
+{
+	int map_row;
+	int map_column;
+	int wall;
+
+	map_row = row;
+	map_column = column;
+	//printf("before map row\n");
+	//printf("map row is %s\n", map[map_row]);
+	if(ft_strlen1(map[map_row]) - 1 <= column)
+		return(0);
+	map_column ++;
+	wall = 0;
+	while(wall == 0 && map[row][column] != '\0')
+	{
+		if(is_end_map_char(map[row][column])== 1)
+			return(0);
+		if(map[row][column] == '1')
+			wall = 1;
+		column++;
+	}
+	return(wall);
+
+}
+
 //return number of rows of map
 int get_map_rows(char **map)
 {
@@ -53,6 +119,20 @@ int all_direction_checker(char dot, int xd, int yd, char **map)
 	//printf("map rows is %d\n", map_rows);
 	if(is_valid_char(dot) != 1)
 		return(0);
+	if(need_check(dot))
+	{
+		int value;
+		value = right_check(dot, xd, yd, map);
+		if(value == 0)
+			return(0);
+		//printf("karakter %c need check\n", dot);
+		//checkleft
+		//chekkright
+		//checkdown
+		//checkup
+	}
+	else 
+		//printf("karakter %c do not need check\n", dot);
 	return (1);
 }
 
@@ -81,13 +161,17 @@ int is_map_cased(t_pars *pars)
 		{
 			int value;
 			value = all_direction_checker(map[row][column], row, column, map);
-			printf("map is close = %d\n", value);
+			//printf("map is close = %d\n", value);
 			if(value != 1)
+			{
+				printf("nije zatvoren\n");
 				return(0);
+			}
 			column++;	
 		}
 		row++;
 	}
+	printf("zatvorena mapa\n");
 
 	return (0);
 }
