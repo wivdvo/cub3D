11029fd33a83826b4e1ee74dd1ffabc45200925c
filip <filip.seles@gmail.com>
2024-02-13 19:41:39 +0100

    segfault dirty fix

--- render.c
+++ render.c
@@ -289,7 +289,8 @@ void raycaster(t_cube *cube)
 		for (int y = cube->draw_start; y <= cube->draw_end; y++)
 		{
 			//mlx_pixel_put(cube->mlx_ptr, cube->win_ptr, x, y, 0xFF0000);
-			my_mlx_pixel_put(&cube->img, x, y, 0x00FF0000);
+			if(cube->draw_end != 0 && cube->draw_start != 0 )
+				my_mlx_pixel_put(&cube->img, x, y, 0x00FF0000);
 		}
 
 		// for (int y = 50; y <= 200; y++)
