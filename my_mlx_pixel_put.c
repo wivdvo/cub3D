73accec418534c127f7656949ce6452f51eb9429
Filip Seles <fseles@c4r3p4.42vienna.com>
2024-02-22 17:24:57 +0100

    pixel put norm

--- my_mlx_pixel_put.c
+++ my_mlx_pixel_put.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_mlx_pixel_put.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fseles <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 17:23:30 by fseles            #+#    #+#             */
+/*   Updated: 2024/02/22 17:23:40 by fseles           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "headers/cub3d.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
@@ -5,5 +17,5 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	char	*dst;
 
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	*(unsigned int *)dst = color;
 }
