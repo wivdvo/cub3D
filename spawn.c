e53aa03e21ce4db607ccf65a9cf48a1c1bf99fcd
Filip Seles <fseles@c4r3p4.42vienna.com>
2024-02-22 17:42:12 +0100

    spawn norm

--- spawn.c
+++ spawn.c
@@ -12,7 +12,7 @@
 
 #include "headers/cub3d.h"
 
-static void north(t_cube *cube)
+static void	north(t_cube *cube)
 {
 	cube->dir_x = -1;
 	cube->dir_y = 0;
@@ -20,14 +20,15 @@ static void north(t_cube *cube)
 	cube->plane_y = 0.66;
 }
 
-static void west(t_cube *cube)
+static void	west(t_cube *cube)
 {
 	cube->dir_x = 0;
 	cube->dir_y = -1;
 	cube->plane_x = -0.66;
 	cube->plane_y = 0;
 }
-static void south(t_cube *cube)
+
+static void	south(t_cube *cube)
 {
 	cube->dir_x = 1;
 	cube->dir_y = 0;
@@ -35,7 +36,7 @@ static void south(t_cube *cube)
 	cube->plane_y = -0.66;
 }
 
-static void east(t_cube *cube)
+static void	east(t_cube *cube)
 {
 	cube->dir_x = 0;
 	cube->dir_y = 1;
@@ -43,14 +44,14 @@ static void east(t_cube *cube)
 	cube->plane_y = 0;
 }
 
-int is_player(char c)
+int	is_player(char c)
 {
 	if (c == 'N' || c == 'E' || c == 'S' || c == 'W')
 		return (1);
 	return (0);
 }
 
-void set_spawn_direction(char c, t_cube *cube)
+void	set_spawn_direction(char c, t_cube *cube)
 {
 	if (c == 'N')
 		cube->spawn_direction = NORTH;
@@ -62,22 +63,22 @@ void set_spawn_direction(char c, t_cube *cube)
 		cube->spawn_direction = WEST;
 }
 
-void get_spawn_position(t_cube *cube)
+void	get_spawn_position(t_cube *cube)
 {
-	char **map;
+	char	**map;
+	int		i;
+	int		j;
+
 	map = cube->map;
-	int i;
 	i = 0;
-	int j;
 	j = 0;
-	while(map[i] != NULL)
+	while (map[i] != NULL)
 	{
 		j = 0;
-		while(map[i][j] != '\0')
+		while (map[i][j] != '\0')
 		{
-			if(is_player(map[i][j]) == 1)
+			if (is_player(map[i][j]) == 1)
 			{
-				//printf("Player position is %d and %d", i, j);
 				set_spawn_direction(map[i][j], cube);
 				cube->pos_x = i + 0.5;
 				cube->pos_y = j + 0.5;
@@ -89,18 +90,15 @@ void get_spawn_position(t_cube *cube)
 	}
 }
 
-void spawn_player(t_cube *cube)
+void	spawn_player(t_cube *cube)
 {
 	get_spawn_position(cube);
-	//cube->pos_x = 2;
-	//cube->pos_y = 3;
-	
-	if(cube->spawn_direction == NORTH)
+	if (cube->spawn_direction == NORTH)
 		north(cube);
-	else if(cube->spawn_direction == EAST)
+	else if (cube->spawn_direction == EAST)
 		east(cube);
-	else if(cube->spawn_direction == SOUTH)
+	else if (cube->spawn_direction == SOUTH)
 		south(cube);
-	else if(cube->spawn_direction == WEST)
+	else if (cube->spawn_direction == WEST)
 		west(cube);
 }
