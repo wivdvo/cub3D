8cd9925407ceaaebf8706e9f422276d9ee411e1b
Filip Seles <fseles@c4r3p4.42vienna.com>
2024-02-26 11:22:37 +0100

    file name checker

--- main.c
+++ main.c
@@ -12,12 +12,38 @@
 
 #include "headers/cub3d.h"
 
-int	main(void)
+static int	check_file_type(char *name)
+{
+	int	file_len;
+
+	if (name == NULL)
+		return (0);
+	file_len = ft_strlen(name);
+	if (file_len < 5)
+		return (0);
+	file_len--;
+	if (name[file_len] == 'b' && name[file_len - 1] == 'u'
+		&& name[file_len - 2] == 'c' && name [file_len - 3] == '.')
+		return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
 {
 	t_cube	cube;
 
+	if (argc != 2)
+	{
+		put_error("expected only a map in format *.cub");
+		return (1);
+	}
+	if (check_file_type(argv[1]) != 1)
+	{
+		put_error("Invalid file name/type");
+		return (1);
+	}
 	init_pars_struct(&cube);
-	parsing(&cube, "file.cub");
+	parsing(&cube, argv[1]);
 	render(&cube);
 	pars_exit(&cube, NULL);
 	return (0);
